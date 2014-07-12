81918098eb9f17598fa02dfd89bc2cc02fc876a5
Przemyslaw Bobak <bobak.art@gmail.com>
2014-07-12 13:38:56 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-kudos-app
+Kudos Service App
 =========
+
+This is a simple kudos service app that counts button hits sent through AJAX and retains them in a JSON file on each POST request.
---
+++ app.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var kudosService = require('./lib/kudos');
+var app = express();
+
+app.use('/', router);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.json({
+        path: req.query.path,
+        count: kudosService.getCount(req.query.path)
+    });
+});
+
+/* POST statistics. */
+router.post('/', function(req, res) {
+    kudosService.updateCount(req.query.path, function(err, counter){
+        res.json({
+            path: req.query.path,
+            count: counter
+        });
+    });
+});
+
+module.exports = app;
