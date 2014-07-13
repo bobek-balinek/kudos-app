cb56e797ac877c53416205da3a5e9dd8f3f139ab
Przemyslaw Bobak <bobak.art@gmail.com>
2014-07-13 13:34:33 +0100

    Add Cross-domain allow for the main domain

--- app.js
+++ app.js
@@ -44,6 +44,17 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.all('*', function(req, res, next){
+  if (!req.get('Origin')) return next();
+  // use "*" here to accept any origin
+  res.set('Access-Control-Allow-Origin', 'http://desert.fm');
+  res.set('Access-Control-Allow-Methods', 'GET, POST');
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  // res.set('Access-Control-Allow-Max-Age', 3600);
+  if ('OPTIONS' == req.method) return res.send(200);
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res) {
     res.json({
