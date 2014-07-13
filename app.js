96ea4f446c137a57567f205eb2f4f19803e85e72
Przemyslaw Bobak <bobak.art@gmail.com>
2014-07-13 13:42:37 +0100

    refactor cross domain policy

--- app.js
+++ app.js
@@ -45,13 +45,8 @@ app.use(function(err, req, res, next) {
 });
 
 app.all('*', function(req, res, next){
-  if (!req.get('Origin')) return next();
-  // use "*" here to accept any origin
-  res.set('Access-Control-Allow-Origin', 'http://desert.fm');
-  res.set('Access-Control-Allow-Methods', 'GET, POST');
-  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-  // res.set('Access-Control-Allow-Max-Age', 3600);
-  if ('OPTIONS' == req.method) return res.send(200);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 
